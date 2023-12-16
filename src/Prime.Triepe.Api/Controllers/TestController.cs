cc010dc63ca55b4dd2567d57a4ebf677623b4783
Ivan Rusev <ivanrusev03@schoolmath.eu>
2023-12-16 20:25:59 +0200

    chore: ensure github job works by leaving mistake

---
+++ src/Prime.Triepe.Api/Controllers/TestController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Triepe.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> Test()
+        {
+            BadHttpRequestException 
+            return Ok();
+        }
+    }
+}
