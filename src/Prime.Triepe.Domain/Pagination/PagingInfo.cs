88e4c84c717f4cda36a608e708bcd5f99a30ac9b
Ivan Rusev <ivanrusev03@schoolmath.eu>
2023-12-27 01:46:25 +0200

    test(fix): resolve failed test

--- src/Prime.Triepe.Domain/Pagination/PagingInfo.cs
+++ src/Prime.Triepe.Domain/Pagination/PagingInfo.cs
@@ -8,7 +8,7 @@ namespace Triepe.Domain.Pagination
 {
     public class PagingInfo
     {
-        public int Page { get; set; } = 0;
+        public int Page { get; set; } = 1;
         public int Size { get; set; } = 10;
     }
 
